d0f96a3f34be681fb73e00db5dc317d9d0a744be
bayashi1019 <n0bu.1019.n0bu@gmail.com>
2026-03-29 17:00:35 +0900

    IDに"TKY\"を追加、TABキーの修正

--- keyboards/foxtrot/keymaps/default/keymap.c
+++ keyboards/foxtrot/keymaps/default/keymap.c
@@ -30,6 +30,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     unregister_code(KC_LCTL);
 
                     wait_ms(3000);
+                    SEND_STRING("TKY");
+                    tap_code16(JP_YEN);   // ← これが「\」
                     SEND_STRING("057126");
                     wait_ms(500);
                     tap_code(KC_TAB);
@@ -83,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_fullsize_iso(     
         SCR_ESC,  KC_LGUI,  WINR,     PASS,     KC_LEFT,  KC_UP,    KC_DOWN,  KC_RGHT,  KC_DEL,
         JP_ZKHK,  JP_1,     JP_2,     JP_3,     JP_4,     JP_5,     JP_6,     JP_7,     JP_8,     JP_9,     JP_0,      JP_MINS,   JP_CIRC,   JP_YEN,    KC_BSPC,
-        KC_LALT,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      JP_AT,     JP_LBRC,   KC_ENT,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      JP_AT,     JP_LBRC,   KC_ENT,
         KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     JP_SCLN,   JP_COLN,   JP_RBRC,
         KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     JP_COMM,  JP_DOT,   JP_SLSH,   JP_BSLS,   KC_RSFT,
         MS_BTN1,  KC_SPC,   MS_BTN2
